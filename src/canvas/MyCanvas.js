007c298251321caeabf9213971356ae6d86cee06
King <d3po.techengineer@gmail.com>
2018-09-26 10:34:45 -0400

    add image to canvas

--- src/canvas/MyCanvas.js
+++ src/canvas/MyCanvas.js
@@ -18,19 +18,19 @@ class myCanvas extends React.Component {
   // this is a function component - it takes properties it doesn't have its own state
   componentDidMount() {
     console.log("myCanvas mounted");
-    // var canvas = document.getElementById('canvas'),
-    // context = canvas.getContext('2d'),
-    // image = new Image();
-    // image.src = 'https://i.ytimg.com/vi/hjVN4KQY_Ko/maxresdefault.jpg';
-    // image.onload = function(e) {
-    // context.drawImage(image, 0, 0);
-    // };
+    var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d'),
+    image = new Image();
+    image.src = 'https://i.ytimg.com/vi/hjVN4KQY_Ko/maxresdefault.jpg';
+    image.onload = function(e) {
+    context.drawImage(image, 0, 0);
+    };
 
   }
   render(){
       return (
         <div>
-        <h1>New canvas element</h1>
+        <h1>Zoom 1</h1>
         <canvas id="canvas" className="canvas"></canvas>
         </div>
       );

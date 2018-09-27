c65d9b56dcb467b668442eeee2d746dd2b3b0de1
King <d3po.techengineer@gmail.com>
2018-09-27 14:05:56 -0400

    circle exploration create multiple circles on a single canvas stroke and fill styles added text and colors

--- src/canvas/MyCanvas.js
+++ src/canvas/MyCanvas.js
@@ -59,17 +59,62 @@ class myCanvas extends React.Component {
       let boss = this.state;
       boss.context.clearRect(0,0,boss.canvas.width,boss.canvas.height);
 
-      this.drawCircle();
+      this.drawCircles();
     }
 
-    drawCircle = () => {
+    drawCircles = () => {
       let boss = this.state;
+
+      let point_zero = 0*Math.PI;
+      let half_circle = 1*Math.PI;
+      let full_circle = 2*Math.PI;
+      let t_qtr_circle = .5*Math.PI;
+      let qtr_circle = 1.5*Math.PI;
+      let clockwise = true;
+
+      let startpoint = point_zero,
+      endpoint = t_qtr_circle,// this seems to tell canvas where to start the arc
+      center_w = boss.canvas.width/2,
+      center_h = boss.canvas.height/2,
+
+      x_coord = center_w,
+      y_coord = center_h,
+
+      full_size_radius = boss.RADIUS,
+      smaller_radius = 100,
+      medium_radius = 200,
+      radius = full_size_radius;
+
+      // circle hint
+      // https://www.w3schools.com/tags/canvas_arc.asp
       boss.context.beginPath();
-      boss.context.arc(boss.canvas.width/2, boss.canvas.height/2, boss.RADIUS, 0,2*Math.PI, true);
+      boss.context.arc(x_coord, y_coord, radius, startpoint, endpoint, clockwise);
+      boss.context.lineWidth=5;
+      boss.context.strokeStyle="red";
       boss.context.stroke();
-      boss.context.fillStyle = "yellow";
+      boss.context.closePath();
+      boss.context.fillStyle = "green";
       boss.context.fill();
-    }
+
+
+      let circles = [
+        {f_color:"yellow",s_color:"blue", x:center_w, y:center_h, radius:medium_radius},
+        {f_color:"blue",s_color:"red" ,x:center_w, y:center_h, radius:smaller_radius}
+      ]
+
+      circles.forEach( (entry) =>{
+
+        boss.context.beginPath();
+        boss.context.arc(entry.x, entry.y, entry.radius, 0, 2*Math.PI, clockwise);
+        boss.context.stroke();
+        boss.context.fillStyle = entry.f_color;
+        boss.context.fill();
+      });
+
+      boss.context.font="50px Courier";
+      boss.context.strokeText("Big smile!",10,100);
+      boss.context.stroke();
+    }// circles
 
   render(){
       return (
